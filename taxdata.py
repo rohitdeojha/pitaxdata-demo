2e30c9fb527423a0aad7cd59ea849afefd5f2821
shekharkhetan <shekharkhetan@gmail.com>
2018-09-25 13:07:56 +0530

    error update

--- taxdata.py
+++ taxdata.py
@@ -1,7 +1,4 @@
 import pandas as pd
-<<<<<<< HEAD
-=======
 
 data = pd.read_csv('SAMPLE_ITR/SAMPLE_AY_2017-18.TXT', sep='|')
-data.to_csv('pitax.csv', index=False)
->>>>>>> upstream/master
+data.to_csv('pit.csv', index=False)
